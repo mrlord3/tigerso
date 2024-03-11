9d01e1d587ce34902dce92bda5d4a29fc5765f75
mrlord3 <mr333ggu@outlook.com>
2024-03-11 04:12:19 +0200

    type any words

---
+++ ZelzalMusic/plugins/tools/must_joinn.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ZelzalMusic import app
+
+@app.on_message(filters.incoming & filters.private, group=-2)
+async def MUST_JOINN_channel(bot: Client, msg: Message):
+    if not "https://t.me/OOOJ30":  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member("OOOJ30", msg.from_user.id)
+        except UserNotParticipant:
+            if "https://t.me/OOOJ30".isalpha():
+                link = "https://t.me/OOOJ30"
+            else:
+                chat_info = await bot.get_chat("OOOJ30")
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"❃︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n❃︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("« ᘜᖇ᥆υρ ᥉᥆υᖇᥴᥱ »", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I m not admin in the MUST_JOINN chat @OOOJ30 !")
