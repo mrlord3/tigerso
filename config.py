1418ecfbceac60f40c98f548ffad29254db11662
mrlord3 <160180670+mrlord3@users.noreply.github.com>
2024-03-11 04:13:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SOURCE-LORD/TEST.git",
+    "https://github.com/mrlord3/tigerso.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
